52a76045f36ff0c1861016b22a2bef2054e6f5c7
John Erdogan <johneerdogan@gmail.com>
2013-09-06 16:35:54 -0400

    all done

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Q3.guessLetters("THUPHEHEY");
+
+            Q2 q2 = new Q2();
+            StringAuditQuestion.Pair[] pairs=q2.auditString("aaaadaaabbdddbbeeeexxxxxxddddderererddfd");
+            foreach (StringAuditQuestion.Pair p in pairs)
+            {
+                Console.WriteLine(p._char + " " + p._occurances);
+
+            }
+            byte[] test = { (byte)0x02, (byte)0x67, (byte)0x67, (byte)0x83, (byte)0xFF,(byte)0x83, (byte)0xFF, (byte)0x00};
+            byte[] output;
+            Q1.DecompressRLE(test, out output);
+            Console.WriteLine();
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(output[i]);
+            }*/
+           
+        }
+
+    }
+}
---
+++ Q1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Q1
+    {
+        public static bool DecompressRLE(byte[] input, out byte[] output)
+        {
+            try
+            {
+                List<byte> decompress = new List<byte>();
+                int counter = 0;
+                while (true)
+                {
+                    byte a = input[counter];
+                    int value = a & 0x80;
+                    if ((a & 0x80) == 128)// repeat 
+                    {
+                        int numRepeats = a & 0x7f;
+                        byte repeat = input[counter+1];
+                        for (int i = 0; i < numRepeats; i++)
+                        {
+                            decompress.Add(repeat);
+                        }
+                        counter += 2;
+                    }
+                    else if ((a & 0x80) == 0) // non repeating
+                    {
+                        int nonRepeat = a & 0x7f;
+                        //not really sure about repeating, if it's the immediate one before or if it repeats at all. So I did if it repeats at all
+                        HashSet<byte> checkNonRepeat = new HashSet<byte>();
+                        for (int i = counter + 1; i < counter + 1 + nonRepeat;i++ )
+                        {
+                            if (!checkNonRepeat.Contains(input[i]))
+                            {
+                                decompress.Add(input[i]);
+                                checkNonRepeat.Add(input[i]);
+                            }
+                            else
+                            {
+                                output = null;
+                                return false;
+                            }
+                        }
+                        counter += nonRepeat+1;
+                    }
+                    if (input[counter] == 0x00)
+                    {
+                        output = decompress.ToArray();
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                output=null;
+                return false;
+            }
+
+            
+        }
+    }
+}
